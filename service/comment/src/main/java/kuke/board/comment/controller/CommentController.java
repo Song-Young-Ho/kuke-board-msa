c101ef8ae15e8e5859afbef9bce6cea82456c4fb
Song-Young-Ho <sng4929@naver.com>
2025-08-17 18:42:47 +0900

    feat: 댓글 최대 2depth

--- service/comment/src/main/java/kuke/board/comment/controller/CommentController.java
+++ service/comment/src/main/java/kuke/board/comment/controller/CommentController.java
@@ -2,9 +2,12 @@ package kuke.board.comment.controller;
 
 import kuke.board.comment.service.CommentService;
 import kuke.board.comment.service.request.CommentCreateRequest;
+import kuke.board.comment.service.response.CommentPageResponse;
 import kuke.board.comment.service.response.CommentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +27,24 @@ public class CommentController {
     @DeleteMapping("/v1/comments/{commentId}")
     public void delete(@PathVariable Long commentId) {
         service.delete(commentId);
+    }
+
+    @GetMapping("/v1/comments")
+    public CommentPageResponse readAll(
+            @RequestParam("articleId") Long articleId,
+            @RequestParam("page") Long page,
+            @RequestParam("pageSize") Long pageSize
+    ) {
+        return service.readAll(articleId, page, pageSize);
+    }
+
+    @GetMapping("/v1/comments/infinite-scroll")
+    public List<CommentResponse> readAll(
+            @RequestParam("articleId") Long articleId,
+            @RequestParam(value = "lastParentCommentId", required = false) Long lastParentCommentId,
+            @RequestParam(value = "lastCommentId", required = false) Long lastCommentId,
+            @RequestParam("pageSize") Long pageSize
+    ) {
+        return service.readAll(articleId, lastParentCommentId, lastCommentId, pageSize);
     }
 }
--- service/comment/src/main/java/kuke/board/comment/service/CommentService.java
+++ service/comment/src/main/java/kuke/board/comment/service/CommentService.java
@@ -4,10 +4,13 @@ import jakarta.transaction.Transactional;
 import kuke.board.comment.entity.Comment;
 import kuke.board.comment.repository.CommentRepository;
 import kuke.board.comment.service.request.CommentCreateRequest;
+import kuke.board.comment.service.response.CommentPageResponse;
 import kuke.board.comment.service.response.CommentResponse;
 import kuke.board.common.snowflake.Snowflake;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static java.util.function.Predicate.not;
 
@@ -49,6 +52,25 @@ public class CommentService {
                         delete(comment);
                     }
                 });
+    }
+
+    public CommentPageResponse readAll(Long articleId, Long page, Long pageSize) {
+        return CommentPageResponse.of(
+                repository.findAll(articleId, (page - 1) * pageSize, pageSize).stream()
+                            .map(CommentResponse::from)
+                            .toList(),
+                repository.count(articleId, PageLimitCalculator.calculatePageLimit(page, pageSize, 10L))
+        );
+    }
+
+    public List<CommentResponse> readAll(Long articleId, Long lastParentCommentId, Long lastCommentId, Long limit) {
+        List<Comment> comments = lastParentCommentId == null || lastCommentId == null ?
+                repository.findAllInfiniteScroll(articleId, limit) :
+                repository.findAllInfiniteScroll(articleId, lastParentCommentId, lastCommentId, limit);
+
+        return comments.stream()
+                .map(CommentResponse::from)
+                .toList();
     }
 
     private boolean hasChildren(Comment comment) {
