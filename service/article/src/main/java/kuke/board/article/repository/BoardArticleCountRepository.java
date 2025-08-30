7742f17d54fac99b8b1a316f49e7bd3137dae2fd
Song-Young-Ho <sng4929@naver.com>
2025-08-30 15:30:29 +0900

    feat: article count

---
+++ service/article/src/main/java/kuke/board/article/repository/BoardArticleCountRepository.java
@@ -0,0 +1,19 @@
+package kuke.board.article.repository;
+
+import kuke.board.article.entity.BoardArticleCount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BoardArticleCountRepository extends JpaRepository<BoardArticleCount, Long> {
+    @Modifying
+    int increase(@Param("articleId") Long articleId);
+
+    @Query(
+            value = "update board_article_count set article_count = article_count - 1 where board_id = :board_id",
+            nativeQuery = true
+    )
+    @Modifying
+    int decrease(@Param("boardId") Long boardId);
+}
